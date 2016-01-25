688453f5f5c7780bd8762b760ba19c10730f33d0
jinseokYeom <jinyeom95@gmail.com>
2016-01-25 17:38:21 -0600

    minor changes

--- game AI/neural_network.py
+++ game AI/neural_network.py
@@ -31,25 +31,30 @@ class NNetwork:
     # create list of number of neurons in each layer
     def get_layer_data(self):
         l_data = []
+
         for _ in range(c.nnet['n_hidden_layers']):
             l_data.append(c.nnet['n_hl_neurons'])
         l_data.append(c.nnet['n_outputs'])
+
         return l_data
 
     # recursively update the neural network
     def update(self, inputs, counter=0, l_counter=0):
-        if l_counter == len(self.l_data):
-            return inputs
+        if l_counter == len(self.l_data): return inputs
         else:
             outputs = []
+
             for _ in range(self.l_data[l_counter]):
                 net_input = 0.0
+
                 for val in inputs:
                     net_input += self.weights[counter] * val
                     counter += 1
+
                 net_input += self.weights[counter] * c.nnet['bias']
                 counter += 1
                 outputs.append(self.sigmoid(net_input))
+
             return self.update(outputs, counter, l_counter + 1)
 
     # sigmoid function
--- game AI/util.py
+++ game AI/util.py
@@ -4,8 +4,11 @@
 def quicksort(agents):
     if len(agents) > 1:
         pivot = agents[0]
+        
         larger = [a for a in agents if a.fitness > pivot.fitness]
         equal = [a for a in agents if a.fitness == pivot.fitness]
         smaller = [a for a in agents if a.fitness < pivot.fitness]
+
         return quicksort(larger) + equal + quicksort(smaller)
+
     return agents
