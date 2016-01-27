23256970623cc19944d999dbb4e370c78ab9d79e
jinseokYeom <jinyeom95@gmail.com>
2016-01-27 11:42:58 -0600

    progress

--- algorithm implementations/ga.py
+++ algorithm implementations/ga.py
@@ -25,34 +25,49 @@ class GA:
     def set_l_dna(self, n):
         self.l_dna = n
 
-    def gen_dna(self):
-        return [r.random() > 0.5 for _ in range(self.l_dna)]
+    def gen_DNA(self):
+        dna = ""
+
+        for _ in range(self.l_dna):
+            if r.random() > 0.5: dna += "1"
+            else: dna += "0"
+
+        return dna
 
     def init_population(self):
         return [self.gen_dna() for _ in range(self.n_pop)]
 
-    def t_selection(self):
+    def t_selection(self, fitness):
         best = r.choice(self.population)
         for _ in range(n_pop - 1):
             rand = r.choice(self.population)
-            if rand.fitness > best.fitness:
+            if fitness(rand) > fitness(best):
                 best = rand
 
         return best
 
-    def mutation(self, dna):
-        return [b if r.random() < self.p_mut else not b for b in dna]
+    def mutation(dna):
+        mutated = ""
 
-    def u_crossover(self, dna_1, dna_2):
-        c_dna_1 = []
-        c_dna_2 = []
+        for b in dna:
+            if r.random() < self.p_mut: mutated += b
+            else:
+                if b == "1": mutated += "0"
+                else: mutated += "1"
+
+        return mutated
+
+    def u_crossover(dna_1, dna_2):
+        c_dna_1 = ""
+        c_dna_2 = ""
 
         for i in range(len(dna_1)):
+
             if r.random() < self.p_xover:
-                c_dna_1.append(dna_1[i])
-                c_dna_2.append(dna_2[i])
+                c_dna_1 += dna_1[i]
+                c_dna_2 += dna_2[i]
             else:
-                c_dna_1.append(dna_2[i])
-                c_dna_2.append(dna_1[i])
+                c_dna_1 += dna_2[i]
+                c_dna_2 += dna_1[i]
 
         return c_dna_1, c_dna_2
--- game AI/playground/dna_representation.c
+++ game AI/playground/dna_representation.c
@@ -5,14 +5,13 @@
     Since a char in C is represented with 8 bits, it can be used a
     piece of DNA with an precision of (1 / 2^8).
 */
-int mutate(char d) {
-    char r;
-    time_t t;
+char mutate(char ch) {
+    char m_ch;
 
-    srand((unsigned) time(&t));
+    m_ch=0x00;
+    for i=0;
 
-    r = 'A' + rand() % 10
-
+    return
 }
 
 int main() {
