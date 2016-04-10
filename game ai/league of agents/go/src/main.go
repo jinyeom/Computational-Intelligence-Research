952ca8e845acef147e26cc5e78ba12b8661bc96a
jinseokYeom <jinyeom95@gmail.com>
2016-04-09 21:03:23 -0500

    progress

--- game ai/league of agents/go/src/main.go
+++ game ai/league of agents/go/src/main.go
@@ -1,7 +1,36 @@
 package main
 
 import (
+    "os"
+    "encoding/json"
     "runtime"
     "github.com/go-gl/glfw/v3.1/glfw"
-
 )
+
+func init() {
+    // main() runs on main thread
+    runtime.LockOSThread()
+
+}
+
+func main() {
+    err := glfw.Init()
+    if err != nil {
+        panic(err)
+    }
+    defer glfw.Terminate()
+
+    window, err := glfw.CreateWindow(W_WIDTH, W_HEIGHT, "Testing", nil, nil)
+    if err != nil {
+        panic(err)
+    }
+
+    window.MakeContextCurrent()
+
+    for !window.ShouldClose() {
+
+
+        window.SwapBuffers()
+        glfw.PollEvents()
+    }
+}
